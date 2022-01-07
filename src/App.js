898aacbb1b02746fa446701d91545b731011ac97
frantchessico <tchessico@live.com>
2022-01-07 15:27:42 -0800

    config root

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@
 import './App.css';
 import Header from './components/header/Header';
 import Sidebar from './components/sidebar/Sidebar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Chat from './pages/Chat/Chat';
 
 function App() {
   return (
     <div className="App">
+     <Router>
      <Header />
      <div className='app_body'>
        <Sidebar />
        {/* React-Router -> Chat screen */}
+
+       <Routes>
+         <Route path="/room/:romId" element={<Chat/>} />
+            
+
+         {/* <Route exact path="/" /> */}
+             
+       </Routes>
      </div>
+     </Router>
     </div>
   );
 }
